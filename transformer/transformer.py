Trying to fix vanishing gradients in multi-layer transformer

transformer/transformer.py
@@ -562,11 +562,15 @@ class GlobalSelfAttention(BaseAttention):
         @Returns
         gradient_x: (batch_size, max_sequence_length, d_model)
         """
-
-        print("global", end="  ---  ")
-        # print("global grad max: ", np.max(gradient_output), end="  -----  ")
+        print("\nglobal", end="  ---  ")
+        print("global grad max: ", np.max(gradient_output), end="  -----  ")
         gradient = self.layer_normalisation.backward(gradient_output, learning_rate)
-        # print("max layer-norm: ", np.max(gradient), end="  ----  ")
+        print("max layer-norm: ", np.max(gradient), end="  ----  ")
+
+        """
+        I'm not taking care of the gradients of the residual connections of attention layers
+        and this might be causing vanishing gradients when there are more than one layer
+        """
 
         # As MultiHeadAttention receives 3 inputs (query, key, value), it returns 3 gradients during backpropagation
         gradient_query, gradient_key, gradient_value = self.mha.backward(gradient, learning_rate)
@@ -603,10 +607,15 @@ class CausalSelfAttention(BaseAttention):
         gradient_x: (batch_size, max_sequence_length, d_model)
         """
 
-        print("causal", end="  ---  ")
-        # print("causal grad max: ", np.max(gradient_output), end="  -----  ")
+        print("\ncausal", end="  ---  ")
+        print("causal grad max: ", np.max(gradient_output), end="  -----  ")
         gradient = self.layer_normalisation.backward(gradient_output, learning_rate)
-        # print("max layer-norm: ", np.max(gradient), end="  ----  ")
+        print("max layer-norm: ", np.max(gradient), end="  ----  ")
+
+        """
+        I'm not taking care of the gradients of the residual connections of attention layers
+        and this might be causing vanishing gradients when there are more than one layer
+        """
 
         # As MultiHeadAttention receives 3 inputs (query, key, value), it returns 3 gradients during backpropagation
         gradient_query, gradient_key, gradient_value = self.mha.backward(gradient, learning_rate)
@@ -645,10 +654,15 @@ class CrossAttention(BaseAttention):
         gradient_context: (batch_size, max_sequence_length, d_model)
         """
 
-        print("cross", end="  ----   ")
-        # print("cross grad max: ", np.max(gradient_output), end="  -----  ")
+        print("\ncross", end="  ----   ")
+        print("cross grad max: ", np.max(gradient_output), end="  -----  ")
         gradient = self.layer_normalisation.backward(gradient_output, learning_rate)
-        # print("max layer-norm: ", np.max(gradient), end="  ----  ")
+        print("max layer-norm: ", np.max(gradient), end="  ----  ")
+
+        """
+        I'm not taking care of the gradients of the residual connections of attention layers
+        and this might be causing vanishing gradients when there are more than one layer
+        """
 
         # As MultiHeadAttention receives 3 inputs (query, key, value), it returns 3 gradients during backpropagation
         gradient_query, gradient_key, gradient_value = self.mha.backward(gradient, learning_rate)
@@ -931,14 +945,14 @@ if __name__ == '__main__':
 
     input_vocab_size = 10
     target_vocab_size = 15
-    num_sequences = 5
+    num_sequences = 10
     max_sequence_length = 10
-    epochs = 50
+    epochs = 2
     batch_size = 32
 
     # Model specifications
+    n_layers = 2
     d_model = 128
-    n_layers = 1
     n_heads = 8
     d_ff = d_model * 4
 

transformer/transformer_pytorch.py
@@ -184,7 +184,7 @@ if __name__ == '__main__':
     d_model = 128
     n_layers = 1
     n_heads = 8
-    d_ff = 512
+    d_ff = d_model * 4
 
     input_data = torch.randint(0, input_vocab_size, (num_sequences, max_sequence_length))
     target_data = torch.randint(0, target_vocab_size, (num_sequences, max_sequence_length))
@@ -224,11 +224,11 @@ if __name__ == '__main__':
     logits = transformer((input_data, target_data))
     probabilities = nn.functional.softmax(logits, dim=-1).detach().numpy()
 
-    # for probabilities_ in probabilities:
-    #     for p in probabilities_:
-    #         print(p, "  --------   ", np.max(p))
-    #     print("\n")
-    # print("\n\n")
+    for probabilities_ in probabilities:
+        for p in probabilities_:
+            print(p, "  --------   ", np.max(p))
+        print("\n")
+    print("\n\n")
 
     correct_generated_tokens = 0
 